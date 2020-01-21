ec112efc470f19e06b55edc074f67d3b6baf8ebd
gaollard <gaollard@foxmail.com>
2020-01-21 14:58:33 +0800

    clean dict

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-// /**
-//  * @format
-//  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src/xz/App';
-
-// import App from './src/demo/nav-scroll';
-// import App from './src/demo/animated/App2';
-// import App from './src/demo/sticky-header/App';
-// import App from './src/demo/react-navition/App';
-// import App from './src/demo/float-input/App2';
-// import App from './src/demo/react-native-scrollable-tab-view/App1';
-// import App from './src/demo/react-native-tab-view/App1';
-
-// import App from './src/demo/ActivityIndicator/App';
-// import App from './src/widget/Modal/App';
-// import App from './src/demo/amap/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
